18c878fb2ce08c42b93ccfee26d96961144b6793
Stefan Sakalos <stefan.sakalos@tipsport.cz>
2018-09-13 10:15:57 +0200

    First commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'test_package',
+    version = '1.0.0',
+    url = 'none',
+    author = 'Author Name',
+    author_email = 'author@gmail.com',
+    description = 'Description of my package',
+    packages = find_packages(),    
+    install_requires = [],
+)
---
+++ test_package/core_methods.py
@@ -0,0 +1,4 @@
+def fun(x):
+	"""Return `x` incremented by 5.
+	"""
+	return x + 5
